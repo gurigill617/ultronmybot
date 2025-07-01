490b0d28e37656b74fbb6ba31f84da6178d6f229
gurigill617 <letsgoganer@gmail.com>
2025-07-01 08:36:10 +0300

    upload

---
+++ utils/recommendations.js
@@ -0,0 +1,26 @@
+export const recommendations = {
+  movie: {
+    action: {
+      happy: ['The Avengers', 'Guardians of the Galaxy'],
+      sad: ['Logan', 'The Pursuit of Happyness'],
+      intense: ['Inception', 'Mad Max: Fury Road']
+    },
+    romance: {
+      happy: ['La La Land', 'The Notebook'],
+      sad: ['A Walk to Remember', 'Titanic'],
+      intense: ['Before Sunrise', 'Blue Valentine']
+    }
+  },
+  anime: {
+    action: {
+      happy: ['My Hero Academia', 'One Punch Man'],
+      sad: ['Attack on Titan', 'Demon Slayer'],
+      intense: ['Jujutsu Kaisen', 'Chainsaw Man']
+    },
+    romance: {
+      happy: ['Toradora!', 'Your Name'],
+      sad: ['Clannad: After Story', 'I Want to Eat Your Pancreas'],
+      intense: ['Kaguya-sama: Love is War', 'Horimiya']
+    }
+  }
+};
